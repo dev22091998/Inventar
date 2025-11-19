d1d006c715ac6521105f2402dbef83d6a2bdf770
Abdurakhmon <88817662+dev22091998@users.noreply.github.com>
2025-11-19 16:53:11 +0500

    Update api.js

--- inventar/frontend/src/services/api.js
+++ inventar/frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  //baseURL: "http://localhost:5000/api"
   // baseURL: "http://172.16.252.187:5000/api"
+   baseURL = 'https://172.16.253.180:5000'  //Linux serveri
 });
 
 export default API;
